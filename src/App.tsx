1151898d72781a3df0a0a665c6bc6e98b127bef5
Bhumika-Donga <bhumidonga25@gmail.com>
2025-04-02 14:51:31 +0530

    routing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './store/store';
 import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
 import './App.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="min-h-screen bg-gray-100">
-        <div className="container mx-auto py-8">
-          <TaskList />
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-100">
+          <Routes>
+            <Route path="/" element={<TaskList />} />
+            <Route path="/create" element={<TaskForm />} />
+            <Route path="/update/:id" element={<TaskForm />} />
+          </Routes>
         </div>
-      </div>
+      </BrowserRouter>
     </Provider>
   );
 }
